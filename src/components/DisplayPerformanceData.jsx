1ddc1552ab418d5394b37ce2b1570b9fe8ec15a4
odeane_croney <odeane_croney@yahoo.com>
2020-07-04 20:57:33 +0200

    adding line graph

--- src/components/DisplayPerformanceData.jsx
+++ src/components/DisplayPerformanceData.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { getData, saveData  } from '../modules/performanceData';
+import { getData, saveData } from '../modules/performanceData';
+import { Line } from 'react-chartjs-2';
 
 class DisplayPerformanceData extends Component {
   state = {
@@ -28,11 +29,31 @@ class DisplayPerformanceData extends Component {
     let dataIndex;
 
     if (this.state.performanceData != null) {
+      
+      const distances = []
+      const labels = []
+
+      this.state.performanceData.forEach(entry => {
+        distances.push(entry.data.distance)
+        labels.push(entry.data.message)
+      })
+      
+      const data = {
+        labels: labels,
+        datasets: [{
+          data: distances,
+          label: "saved distances",
+          backgroundColor: 'rgba(16, 29, 110, 0.72)',
+          borderColor: 'rgba(16, 29, 110, 0.96)',
+          borderCapStyle: 'butt',
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointBorderColor: 'rgb(47, 68, 207)'
+        }]
+      }
       dataIndex = (
         <div>
-          {this.state.performanceData.map(item => {
-            return <div key={item.id}>{item.data.message}</div>
-          })}
+          <Line data={data} />
         </div>
       )
     }
--- src/components/InputFields.jsx
+++ src/components/InputFields.jsx
@@ -1,42 +1,19 @@
 import React from "react";
-import {Header, Image, Form} from "semantic-ui-react"
-
-const options = [
-  { key: 'm', text: 'Male', value: 'male' },
-  { key: 'f', text: 'Female', value: 'female' },
-]
-
 
 const InputFields = ({ onChangeHandler }) => {
   return (
     <>
-    <Header as="h1" textAlign="center" color='grey'block>
-    <Image src='https://services.garmin.com/appsLibraryBusinessServices_v0/rest/apps/2cf376ad-a240-41fb-bb2b-528f54270baa/icon/b4bfdf57-5fc9-405b-88f2-8e96c2b1f3c8'/>
-      Track your fitness by the Cooper Test
-      </Header>
-  
-      <Form>
-      <Form.Group widths='equal'> 
-      <Form.Input fluid label='Distance'
-        placeholder='Enter distance in meter' 
-        onChange={onChangeHandler} 
-        name="distance" id="distance" 
-        />
+      <lable>Distance</lable>
+      <input onChange={onChangeHandler} name="distance" id="distance"></input>
 
-      
-      <Form.Select
-            fluid
-            label='Gender'
-            options={options}
-            placeholder='Enter gender...'
-          />
-      
-      <Form.Input fluid label='Age' placeholder='Enter age...' onChange={onChangeHandler} name="age" id="age"/>
-    </Form.Group>
-    </Form>
-  
+      <select onChange={onChangeHandler} name="gender" id="gender">
+        <option value="female">Female</option>
+        <option value="male">Male</option>
+      </select>
+
+      <label>Age</label>
+      <input onChange={onChangeHandler} name="age" id="age"></input>
     </>
   );
 };
-
 export default InputFields;
