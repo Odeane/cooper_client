8cb3ff370d99d9b7f43dad5131bad6e08d9fb774
odeane_croney <odeane_croney@yahoo.com>
2020-07-05 00:39:34 +0200

    Adding style to form

--- src/components/InputFields.jsx
+++ src/components/InputFields.jsx
@@ -1,19 +1,35 @@
 import React from "react";
+import { Form, Input} from 'semantic-ui-react'
 
 const InputFields = ({ onChangeHandler }) => {
   return (
-    <>
-      <lable>Distance</lable>
-      <input onChange={onChangeHandler} name="distance" id="distance"></input>
-
-      <select onChange={onChangeHandler} name="gender" id="gender">
+    <Form>
+      
+      <Form.Group widths='equal'>
+      
+      
+        <Form.Input fluid label='Distance' placeholder='Distance' onChange={onChangeHandler} name="distance" id="distance"/>
+          
+      
+        
+       
+        
+        <Form.Input fluid label='Age' placeholder='Age'  onChange={onChangeHandler} name="age" id="age"/>
+     
+      </Form.Group>
+      
+    
+        
+      
+        <select onChange={onChangeHandler} name="gender" id="gender">
         <option value="female">Female</option>
         <option value="male">Male</option>
-      </select>
-
-      <label>Age</label>
-      <input onChange={onChangeHandler} name="age" id="age"></input>
-    </>
+          </select>
+      
+        
+        
+      
+    </Form>
   );
 };
 export default InputFields;
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,16 +1,46 @@
 import React from "react";
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 const LoginForm = ({ submitFormHandler }) => {
+
+
+
     return (
-        <form onSubmit ={submitFormHandler} id="login-form">
-            <label>Email</label>
-            <input name="email" type="email" id="email"></input>
+        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+            <Grid.Column style={{ maxWidth: 450 }}>
+                <Header as='h2' color='green' textAlign='center'>
+                    <Image src='https://codegreenhealth.com/wp-content/uploads/sites/85/2018/01/cardiogram2.png' /> Log-in to your account
+                </Header>
 
-            <label>Password</label>
-            <input name="password" type="password" id="password"></input>
 
-            <button id="submit">Submit</button>
-        </form>
+
+                <Form size='large' onSubmit={submitFormHandler} id="login-form">
+                    <Segment stacked>
+
+                        <label>Email</label>
+                        <input fluid icon='user' iconPosition='left' placeholder='E-mail address' name="email" type="email" id="email"></input>
+
+                        <label>Password</label>
+                        <input fluid
+                            icon='lock'
+                            iconPosition='left'
+                            placeholder='Password'
+                            name="password" type="password" id="password"></input>
+
+
+                        <Button color='green' fluid size='large' id="submit">Submit</Button>
+                    </Segment>
+
+
+                </Form>
+
+
+
+                <Message>
+                    <p>  Ready? Lets go</p>
+                </Message>
+            </Grid.Column>
+        </Grid>
     );
 };
 
